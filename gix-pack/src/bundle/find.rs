63bc60741c745ee35043fd217d226c2292c1af55
Christoph Rüßler <christoph.ruessler@mailbox.org>
2026-03-31 20:38:06 +0200

    Adapt to changes in `gix-object`

--- gix-pack/src/bundle/find.rs
+++ gix-pack/src/bundle/find.rs
@@ -58,6 +58,7 @@ impl crate::Bundle {
                     gix_object::Data {
                         kind: r.kind,
                         data: out.as_slice(),
+                        hash_len: self.pack.object_hash().len_in_bytes(),
                     },
                     crate::data::entry::Location {
                         pack_id: self.pack.id,
